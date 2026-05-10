1b3975397f42fbf1ad123adb7635f676bfeb3fcf
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-10 17:26:19 +0530

    update something 2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1> Hey Parinika from Delhi NCR</h1>
+      <h1> Suna Hai do you love me?</h1>
       <Link href="/features">Features</Link>
       <Link href="/about">About</Link>
       <Link href="/pricing">Pricing</Link>
